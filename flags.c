fd49659d88197a82eaf8899b2c1812914092fdf7
Gilbert Mutai <reyandkyle@gmail.com>
2023-04-26 01:18:31 +0300

    Updated

--- flags.c
+++ flags.c
@@ -18,3 +18,16 @@ for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 
 for (j = 0; FLAGS_CH[j] != '\0'; j++)
 {
+if (format[curr_i] == FLAGS_CH[j])
+{
+flags |= FLAGS_ARR[j]; /* Set flag bit using bitwise OR */
+break;
+}
+}
+if (FLAGS_CH[j] == 0)
+break;
+}
+
+*i = curr_i - 1;
+return (flags);
+}
